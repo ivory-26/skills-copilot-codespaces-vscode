2e317b8231afad2666b3b8f38ff8f0011e553c2f
Sahil Karpe <167101357+ivory-26@users.noreply.github.com>
2024-12-15 10:53:20 +0000

    new commit

--- comments.js
+++ comments.js
@@ -1,51 +1,27 @@
 //create web server
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
-var comments = [];
-var server = http.createServer(function(request, response) {
-    var parseUrl = url.parse(request.url);
-    var pathname = parseUrl.pathname;
-    if (pathname === '/') {
-        fs.readFile('./index.html', function(err, data) {
-            if (err) {
-                response.writeHead(404, {
-                    'Content-Type': 'text/html'
-                });
-                response.end('<h1>404 Not Found</h1>');
-            } else {
-                response.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                response.end(data);
-            }
-        });
-    } else if (pathname === '/getComments') {
-        response.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        response.end(JSON.stringify(comments));
-    } else if (pathname === '/addComment') {
-        var data = '';
-        request.on('data', function(chunk) {
-            data += chunk;
-        });
-        request.on('end', function() {
-            var comment = querystring.parse(data);
-            comments.unshift(comment);
-            response.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            response.end(JSON.stringify(comment));
-        });
-    } else {
-        response.writeHead(404, {
-            'Content-Type': 'text/html'
-        });
-        response.end('<h1>404 Not Found</h1>');
-    }
-});
-server.listen(3000, function() {
-    console.log('Server is running...');
-});
+
+//create server
+http.createServer(function(req, res) {
+  //read file
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
+//create server
+http.createServer(function(req, res) {
+  //read file
+  fs.readFile('comments.json', function(err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.write(data);
+    return res.end();
+  });
+}).listen(8081);
